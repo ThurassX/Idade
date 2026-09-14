2f9947e14bb850eb2eb5828fcf718fa00df31688
ThurasX <arturcustodio02@gmail.com>
2026-09-14 23:46:52 +0100

    gravado

--- src/main.java
+++ src/main.java
@@ -9,12 +9,16 @@
  * @author Thurass
  */
 public class main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        byte idade=10;
+        if(idade<=10){
+            System.out.println("crianca");
+        }else if(idade<=17){
+            System.out.println("adolescente");}
     
+}
 }
